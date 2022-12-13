44fcb078e8501605789a2688153354ed2801a0a2
PreciousAnyi <jkpresh@gmail.com>
2022-12-12 20:18:51 -0800

    added some functionalities

--- app/src/main/java/ps/room/redcard/LoginFragment.kt
+++ app/src/main/java/ps/room/redcard/LoginFragment.kt
@@ -25,19 +25,13 @@ import androidx.navigation.fragment.findNavController
 class LoginFragment : Fragment() {
 
     private lateinit var button: Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
-
-        button = view.findViewById(R.id.loginB)
-
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
